2ab75e9cdc8fb879b3ba8f6aa98538261af7d43c
tipperr <ciaranenator@gmail.com>
2024-01-03 17:57:29 -0500

    Initial Release of WeSplit
    WeSplit is a basic tip calculator and check splitting app.

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Ciaran Murphy on 12/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var grandTotal: Double{
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        return grandTotal
+    }
+    
+    var totalPerPerson: Double{
+        let peopleCount = Double(numberOfPeople + 2)
+        //let tipSelection = Double(tipPercentage)
+        //let tipValue = checkAmount / 100 * tipSelection
+        //let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        NavigationStack{
+            Form{
+                Section{
+                    TextField("Check Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section("Tip Percentage"){
+                    Picker("Tip Percentage", selection: $tipPercentage){
+                        ForEach(/*tipPercentages, id: \.self*/0..<101){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section ("Total With Tip"){
+                    Text(grandTotal, format:. currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                Section ("Total Per Person"){
+                    Text(totalPerPerson, format:. currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done"){
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+    }
+
+#Preview {
+    ContentView()
+}
