303ce29361589e3d08bcdb08abb45c51da3b7416
tipperr <ciaranenator@gmail.com>
2024-01-06 08:29:11 -0500

    Exploring Swift Views and Modifiers

---
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Ciaran Murphy on 1/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        Text("Hello, World!")
+            /*.padding()
+            .background(.red)
+            .padding()
+            .background(.blue)
+            .padding()
+            .background(.green)
+            .padding()
+            .background(.yellow)*/
+        
+        /*Button("Hello World"){
+            
+        }
+
+        .frame(width: 200, height: 200)
+        .background(.red)*/
+        //.frame(maxWidth: .infinity, maxHeight: .infinity)
+        //.background(.red)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Ciaran Murphy on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
