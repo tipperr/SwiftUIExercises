0d4eccab5bdc25d48d893dc5a0dfc50422e4ebb4
tipperr <ciaranenator@gmail.com>
2024-01-12 07:03:48 -0500

    Finished Animations

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -7,17 +7,110 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition{
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
 struct ContentView: View {
+    
+    let letters = Array("Hello SwiftUI")
+    
+    @State private var dragAmount = CGSize.zero
     
     @State private var animationAmount = 0.0
     
     @State private var enabled = false
     
+    @State private var isShowingRed = false
+    
     var body: some View {
         
-        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+        
+        //Custom transitions
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+            
+            if isShowingRed{
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
+        }
+        
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
+        
+        //Animating transitions
+        /*VStack{
+            Button("Tap me"){
+                withAnimation{
+                    isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
+        }*/
+        
+        //Animating gestures:
+        /*HStack(spacing: 0){
+            ForEach(0..<letters.count, id:\.self) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.linear.delay(Double(num)/20), value: dragAmount)
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation}
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+            )*/
+    }
+}
+        
+        //Animating card:
+        /*LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
             .frame(width: 300, height: 200)
             .clipShape(.rect(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged{ dragAmount = $0.translation}
+                    .onEnded{ _ in
+                        withAnimation(.bouncy){
+                            dragAmount = .zero}
+                    }
+            )*/
+            //.animation(.bouncy, value: dragAmount)
         
         //From controlling the animation stack
         /*Button("Tap me"){
@@ -95,8 +188,6 @@ struct ContentView: View {
         .onAppear {
             animationAmount = 2
         }*/
-    }
-}
 
 #Preview {
     ContentView()
