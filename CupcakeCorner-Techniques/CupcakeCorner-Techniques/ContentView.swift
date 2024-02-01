8b253764377d13100850ae4e05c3530aad853fba
tipperr <ciaranenator@gmail.com>
2024-02-01 09:02:33 -0500

    Haptics studies

--- CupcakeCorner-Techniques/CupcakeCorner-Techniques/ContentView.swift
+++ CupcakeCorner-Techniques/CupcakeCorner-Techniques/ContentView.swift
@@ -1,59 +1,3 @@
-/*//
-//  ContentView.swift
-//  CupcakeCorner
-//
-
-import SwiftUI
-
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
-struct ContentView: View {
-    @State private var results = [Result]()
-    var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            }
-        }
-        .task {
-            await loadData() // await tells Swift that the function might "go to sleep", meaning it might take a while to complete
-        }
-    }
-    
-    func loadData() async { // async allows the function to wait until the necessary data from the internet is loaded until it's run. Meanwhile, other aspects of the app will still work. Users won't have to wait until it's completed.
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL") // The URL is defined in Apple's iTunes search API.
-            return
-        }
-        
-        do {
-            let (data,_) = try await URLSession.shared.data(from: url) // The data from URL method contains a data instance of the data it found at the url. You must a try await as it may throw an error
-            if let decodedResponse = try?  JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("Invalid data")
-        }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
- */
-
 //
 //  ContentView.swift
 //  CupcakeCorner-Techniques
@@ -61,7 +5,8 @@ struct ContentView_Previews: PreviewProvider {
 //  Created by Ciaran Murphy on 1/31/24.
 //
 
-import SwiftUI
+//Sending and receiving Codable Data:
+/*import SwiftUI
 
 struct Response: Codable {
     var results: [Result]
@@ -111,17 +56,30 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
-}
+}*/
 
-/*struct ContentView: View {
+
+//Loading an image from a remote server:
+ /*import SwiftUI
+ 
+ struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        //AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 3)
+        //AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")){ image in
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")){ phase in
+            if let image = phase.image{
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("There was an error loading the image")
+            } else {
+                ProgressView()
+            }
+        } /*placeholder: {
+            ProgressView()
+        }*/
+        .frame(width: 200, height: 200)
     }
 }
 
@@ -129,3 +87,86 @@ struct ContentView: View {
     ContentView()
 }*/
 
+//Validating and disabling forms:
+/*import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var username = ""
+    @State private var email = ""
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
+    
+   var body: some View {
+       Form {
+           Section {
+               TextField("Username: ", text: $username)
+               TextField("Email: ", text: $email)
+           }
+           
+           Section {
+               Button("Create Account"){
+                   print("Creating Account...")
+               }
+           }
+           .disabled(disableForm)
+       }
+   }
+}
+
+#Preview {
+   ContentView()
+}*/
+
+//haptics:
+import CoreHaptics
+import SwiftUI
+struct ContentView: View {
+    
+    @State private var counter = 0
+    @State private var engine: CHHapticEngine?
+    
+    var body: some View {
+        Button("Tap Count: \(counter)", action: complexSuccess)
+            .onAppear(perform: prepareHaptics)
+            //counter += 1
+            //complexSuccess()
+        //.sensoryFeedback(.increase, trigger: counter)
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            engine = try CHHapticEngine()
+            try? engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        var events = [CHHapticEvent]()
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription)")
+        }
+    }
+}
+
+
+#Preview {
+   ContentView()
+}
