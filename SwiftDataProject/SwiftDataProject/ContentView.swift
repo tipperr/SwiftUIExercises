8a6142ed40a5517103f2814db4fe2c75891d64cc
tipperr <ciaranenator@gmail.com>
2024-02-07 09:24:30 -0500

    Editing SwiftData model objects

--- SwiftDataProject/SwiftDataProject/ContentView.swift
+++ SwiftDataProject/SwiftDataProject/ContentView.swift
@@ -4,18 +4,33 @@
 //
 //  Created by Ciaran Murphy on 2/7/24.
 //
-
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query(sort: \User.name) var users: [User]
+    @State private var path = [User]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $path){
+            List(users){ user in
+                NavigationLink(value: user){
+                    Text(user.name)
+                }
+            }
+            .navigationTitle("Users")
+            .navigationDestination(for: User.self){ user in
+                EditUserView(user: user)
+            }
+            .toolbar{
+                Button("Add user", systemImage: "plus"){
+                    let user = User(name: "", city: "", joinDate: .now)
+                    modelContext.insert(user)
+                    path = [user]
+                }
+            }
         }
-        .padding()
     }
 }
 
--- SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Ciaran Murphy on 2/7/24.
 //
-
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +13,6 @@ struct SwiftDataProjectApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
