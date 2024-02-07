57b295aa28d78d960aea186f2a6aa099d0a612de
tipperr <ciaranenator@gmail.com>
2024-02-07 07:59:42 -0500

    Created SwiftDataProject

---
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Ciaran Murphy on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
