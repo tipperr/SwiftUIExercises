528ae95a569fe2516257eb8ea0da6284df2baf19
tipperr <ciaranenator@gmail.com>
2024-02-07 21:51:26 -0500

    Filtering @Query using #Predicate

--- SwiftDataProject/SwiftDataProject/ContentView.swift
+++ SwiftDataProject/SwiftDataProject/ContentView.swift
@@ -9,25 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query(sort: \User.name) var users: [User]
-    @State private var path = [User]()
+    @Query(filter: #Predicate<User> { user in
+        //user.name.contains("R")
+        user.name.localizedStandardContains("R") &&
+        user.city == "London"
+    },
+        sort: \User.name) var users: [User]
+    //Removed for "Filtering @Query using #Predicate"
+    //@State private var path = [User]()
     
     var body: some View {
-        NavigationStack(path: $path){
+        NavigationStack(/*Removed for Filtering @Query using #Predicate: path: $path*/){
             List(users){ user in
-                NavigationLink(value: user){
+                //Removed for "Filtering @Query using #Predicate"
+                //NavigationLink(value: user){
                     Text(user.name)
-                }
+                //}
             }
             .navigationTitle("Users")
-            .navigationDestination(for: User.self){ user in
+            //Removed for "Filtering @Query using #Predicate"
+            /*.navigationDestination(for: User.self){ user in
                 EditUserView(user: user)
-            }
+            }*/
             .toolbar{
-                Button("Add user", systemImage: "plus"){
-                    let user = User(name: "", city: "", joinDate: .now)
-                    modelContext.insert(user)
-                    path = [user]
+                //Removed for "Filtering @Query using #Predicate"
+                /*Button("Add user", systemImage: "plus"){
+                 let user = User(name: "", city: "", joinDate: .now)
+                 modelContext.insert(user)
+                 path = [user]
+                 }*/
+                Button("Add Sample", systemImage: "plus"){
+                    try? modelContext.delete(model: User.self)
+                    
+                    let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
+                    let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
+                    let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
+                    let fourth = User(name: "Johnny English ", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
+                    
+                    modelContext.insert(first)
+                    modelContext.insert(second)
+                    modelContext.insert(third)
+                    modelContext.insert(fourth)
                 }
             }
         }
