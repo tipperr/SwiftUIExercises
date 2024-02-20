bf2bfc7b34faa485605cddab8a092b096bcdacb7
tipperr <ciaranenator@gmail.com>
2024-02-20 08:14:25 -0500

    CoreImage and ContentUnavailableView integration

--- Instafilter_Techniques/Instafilter_Techniques/ContentView.swift
+++ Instafilter_Techniques/Instafilter_Techniques/ContentView.swift
@@ -5,12 +5,16 @@
 //  Created by Ciaran Murphy on 2/19/24.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
-    @State private var blurAmount = 0.0 /*{
+    @State private var image: Image?
+    //@State private var showingConfirmation = false
+    //@State private var backgroundColor = Color.white
+    //@State private var blurAmount = 0.0 
+    /*{
         didSet{
             print("New value is \(blurAmount)")
         }
@@ -19,7 +23,28 @@ struct ContentView: View {
     
     
     var body: some View {
-        VStack {
+        
+        /*ContentUnavailableView("No Snippets", systemImage: "swift", description: Text("You don't have any saved snippets yet"))*/
+        ContentUnavailableView {
+            Label("No Snippets", systemImage: "swift")
+        } description: {
+            Text("You don't have any saved snippets yet")
+        } actions: {
+            Button("Create Snippet"){
+                
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        
+        /*VStack{
+            image?
+                .resizable()
+                .scaledToFit()
+                .rotationEffect(.degrees(90))
+        }
+        .onAppear(perform: loadImage)*/
+        
+        /*VStack {
             Text("Hello, world!")
                 .blur(radius: blurAmount)
             
@@ -42,7 +67,39 @@ struct ContentView: View {
             } message: {
                 Text("Select a new color")
             }
+        }*/
+    }
+    func loadImage() {
+        //image = Image(.IMG_0027)
+        let inputImage = UIImage(resource: .IMG_0027)
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        //let currentFilter = CIFilter.sepiaTone()
+        //let currentFilter = CIFilter.twirlDistortion()
+        let currentFilter = CIFilter.crystallize()
+        currentFilter.inputImage = beginImage
+
+        let amount = 1.0
+        
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
         }
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
+        }
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
+        }
+        
+        //currentFilter.intensity = 1
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        let uiImage = UIImage(cgImage: cgImage)
+        image = Image(uiImage: uiImage)
     }
 }
 
