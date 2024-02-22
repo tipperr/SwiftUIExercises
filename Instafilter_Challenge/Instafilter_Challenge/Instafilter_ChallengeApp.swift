9d02314a01cc1984048842c4fd6f967938fd96a3
tipperr <ciaranenator@gmail.com>
2024-02-22 09:46:34 -0500

    Started Instafilter_Challenge

---
+++ Instafilter_Challenge/Instafilter_Challenge/Instafilter_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instafilter_ChallengeApp.swift
+//  Instafilter_Challenge
+//
+//  Created by Ciaran Murphy on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Instafilter_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
