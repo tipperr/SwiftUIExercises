42477ae56c6cbe7c13d3cb37dadd0dedd848fcc4
tipperr <ciaranenator@gmail.com>
2024-02-25 18:23:14 -0500

    Added FaceID

--- BucketList—Techniques/BucketList—Techniques/ContentView.swift
+++ BucketList—Techniques/BucketList—Techniques/ContentView.swift
@@ -122,7 +122,7 @@ struct ContentView: View {
      ContentView()
  }*/
 
-import MapKit
+/*import MapKit
 import SwiftUI
 
 struct Location: Identifiable {
@@ -195,6 +195,47 @@ struct ContentView: View {
          }
      }
 }
+
+ #Preview {
+     ContentView()
+ }*/
+
+import LocalAuthentication
+ import SwiftUI
+
+ struct ContentView: View {
+     @State private var isUnlocked = false
+     var body: some View {
+         VStack {
+             if isUnlocked{
+                 Text("Unlocked")
+             } else {
+                 Text("Locked")
+             }
+         }
+         .onAppear(perform: authenticate)
+     }
+     
+     func authenticate(){
+         let context = LAContext()
+         var error: NSError?
+         
+         if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+             let reason = "We need to unlock your data"
+             
+             context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason){ success, authenticationError in
+                 if success{
+                     isUnlocked = true
+                 } else {
+                     
+                 }
+                 
+             }
+         } else {
+             
+         }
+     }
+ }
 
  #Preview {
      ContentView()
