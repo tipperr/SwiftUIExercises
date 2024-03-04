56840ccedb792e687af7df10bdf569f8435dbdf7
tipperr <ciaranenator@gmail.com>
2024-03-04 07:34:25 -0500

    Image labels for accessibility

---
+++ AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Ciaran Murphy on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  AccessibilitySandbox
+//
+//  Created by Ciaran Murphy on 3/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    
+    let labels = [
+        "Tulips",
+        "Frozen Tree Buds",
+        "Sunflowers",
+        "Fireworks"
+    ]
+    
+    @State private var selectedPicture = Int.random(in: 0...3)
+    
+    var body: some View {
+        Image(pictures[selectedPicture])
+            .resizable()
+            .scaledToFit()
+            .onTapGesture {
+                selectedPicture = Int.random(in: 0...3)
+            }
+            .accessibilityLabel(labels[selectedPicture])
+            .accessibilityAddTraits(.isButton)
+            .accessibilityRemoveTraits(.isImage)
+    }
+}
+
+#Preview {
+    ContentView()
+}
