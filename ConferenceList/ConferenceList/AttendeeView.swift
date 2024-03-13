71b658a2e9f58bf0a0ed1460ac68e154b228bba7
tipperr <ciaranenator@gmail.com>
2024-03-13 09:31:31 -0400

    Added Maps

--- ConferenceList/ConferenceList/AttendeeView.swift
+++ ConferenceList/ConferenceList/AttendeeView.swift
@@ -6,11 +6,25 @@
 //
 
 import CoreLocation
+import MapKit
 import PhotosUI
 import SwiftUI
 
 struct AttendeeView: View {
     let attendee: Attendee
+    
+    @State private var mapCameraPosition: MapCameraPosition
+
+        init(attendee: Attendee) {
+            self.attendee = attendee
+            //let coordinate = attendee.location ?? CLLocationCoordinate2D(latitude: 0, longitude: 0)
+            let coordinate = attendee.location?.coordinate ?? CLLocationCoordinate2D(latitude: 0, longitude: 0)
+
+            let codableCoordinate = CodableCoordinate(coordinate: coordinate)
+                    
+            _mapCameraPosition = State(initialValue: .region(MKCoordinateRegion(center: codableCoordinate.coordinate, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))))
+
+        }
     
     var body: some View {
         NavigationStack{
@@ -19,6 +33,17 @@ struct AttendeeView: View {
                     .resizable()
                     .scaledToFit()
                     .frame(width: 400, height: 400)
+                //Text("\(attendee.location)")
+                Map(position: $mapCameraPosition){
+                    /*Annotation(coordinate: attendee.location?.coordinate ?? CLLocationCoordinate2D(latitude: 0, longitude: 0)){
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(.circle)
+                    }*/
+                }
             }
             .navigationTitle(attendee.attendeeName)
             .navigationBarTitleDisplayMode(.inline)
@@ -29,7 +54,7 @@ struct AttendeeView: View {
 struct AttendeeView_Previews: PreviewProvider {
     static var previews: some View {
         let imageData = Data() // Placeholder image data
-        let attendee = Attendee(imageData: imageData, attendeeName: "John Doe")
+        let attendee = Attendee(imageData: imageData, attendeeName: "John Doe", location: CLLocationCoordinate2D(latitude: 37.3347302, longitude: -122.0089189))
         return AttendeeView(attendee: attendee)
     }
 }
--- ConferenceList/ConferenceList/ContentView.swift
+++ ConferenceList/ConferenceList/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import CoreLocation
+import MapKit
 import PhotosUI
 import SwiftUI
 
@@ -14,6 +15,19 @@ struct Attendee: Identifiable, Codable {
     //let image: Image
     let imageData: Data
     var attendeeName: String
+    var location: CodableCoordinate?
+    
+    init(imageData: Data, attendeeName: String) {
+        self.imageData = imageData
+        self.attendeeName = attendeeName
+        self.location = nil
+    }
+    
+    init(imageData: Data, attendeeName: String, location: CLLocationCoordinate2D?) { // Update initializer
+        self.imageData = imageData
+        self.attendeeName = attendeeName
+        self.location = location.map(CodableCoordinate.init)
+    }
 }
 
 class Attendees: ObservableObject {
@@ -46,6 +60,20 @@ class Attendees: ObservableObject {
         }
     }
 }
+    
+struct CodableCoordinate: Codable, Equatable {
+    var latitude: CLLocationDegrees
+    var longitude: CLLocationDegrees
+
+    init(coordinate: CLLocationCoordinate2D) {
+            latitude = coordinate.latitude
+            longitude = coordinate.longitude
+        }
+
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
 
 class LocationFetcher: NSObject, CLLocationManagerDelegate {
     let manager = CLLocationManager()
@@ -66,79 +94,79 @@ class LocationFetcher: NSObject, CLLocationManagerDelegate {
     }
 }
 
-struct ContentView: View {
-    //@State private var conferenceList = []
-    @StateObject private var attendees = Attendees()
-    @State private var conferenceList = [Attendee]()
-    @State private var pickerItem: PhotosPickerItem?
-    @State private var pickerItems = [PhotosPickerItem]()
-    @State private var selectedImage: Image?
-    @State private var selectedImageData: Data?
-    //@State private var selectedImages = [Image]()
-    @State private var attendeeName = "Attendee Name"
-    @State private var showingNameAlert = false
-    @State private var showingAttendee = false
-    let locationFetcher = LocationFetcher()
-    
-    var body: some View {
-        NavigationView{
-            VStack {
-                PhotosPicker("Select a picture", selection: $pickerItem, matching: .images)
-                List{
-                    //ForEach(conferenceList) { attendee in
-                    ForEach(attendees.attendees) { attendee in
-                        NavigationLink(destination: AttendeeView(attendee: attendee)){
-                            HStack {
-                                Image(uiImage: UIImage(data: attendee.imageData) ?? UIImage(systemName: "person.circle.fill")!)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 50, height: 50)
-                                Spacer()
-                                Text(attendee.attendeeName)
-                            }
-                        }
-                    }
-                }
-                
+    struct ContentView: View {
+        //@State private var conferenceList = []
+        @StateObject private var attendees = Attendees()
+        @State private var conferenceList = [Attendee]()
+        @State private var pickerItem: PhotosPickerItem?
+        @State private var pickerItems = [PhotosPickerItem]()
+        @State private var selectedImage: Image?
+        @State private var selectedImageData: Data?
+        //@State private var selectedImages = [Image]()
+        @State private var attendeeName = "Attendee Name"
+        @State private var showingNameAlert = false
+        @State private var showingAttendee = false
+        let locationFetcher = LocationFetcher()
+        
+        var body: some View {
+            NavigationView{
                 VStack {
-                            Button("Start Tracking Location") {
-                                locationFetcher.start()
-                            }
-
-                            Button("Read Location") {
-                                if let location = locationFetcher.lastKnownLocation {
-                                    print("Your location is \(location)")
-                                } else {
-                                    print("Your location is unknown")
+                    PhotosPicker("Select a picture", selection: $pickerItem, matching: .images)
+                    List{
+                        //ForEach(conferenceList) { attendee in
+                        ForEach(attendees.attendees) { attendee in
+                            NavigationLink(destination: AttendeeView(attendee: attendee)){
+                                HStack {
+                                    Image(uiImage: UIImage(data: attendee.imageData) ?? UIImage(systemName: "person.circle.fill")!)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 50, height: 50)
+                                    Spacer()
+                                    Text(attendee.attendeeName)
                                 }
                             }
                         }
-                
-                .onChange(of: pickerItem) { _ in
-                    Task {
-                        if let imageData = try await pickerItem?.loadTransferable(type: Data.self) {
-                            selectedImageData = imageData
-                            showingNameAlert.toggle()
+                    }
+                    
+                    VStack {
+                        Button("Start Tracking Location") {
+                            locationFetcher.start()
+                        }
+                        
+                        Button("Read Location") {
+                            if let location = locationFetcher.lastKnownLocation {
+                                print("Your location is \(location)")
+                            } else {
+                                print("Your location is unknown")
+                            }
                         }
                     }
-                }
-                .alert("Enter attendee's name", isPresented: $showingNameAlert) {
-                    TextField("Attendee Name", text: $attendeeName)
                     
-                    Button("OK") {
-                        if let imageData = selectedImageData {
-                            let attendee = Attendee(imageData: imageData, attendeeName: attendeeName)
-                            //conferenceList.append(attendee)
-                            attendees.attendees.append(attendee)
+                    .onChange(of: pickerItem) { _ in
+                        Task {
+                            if let imageData = try await pickerItem?.loadTransferable(type: Data.self) {
+                                selectedImageData = imageData
+                                showingNameAlert.toggle()
+                            }
                         }
-                        selectedImageData = nil
-                        attendeeName = "Attendee Name"
-                        showingNameAlert = false
+                    }
+                    .alert("Enter attendee's name", isPresented: $showingNameAlert) {
+                        TextField("Attendee Name", text: $attendeeName)
+                        
+                        Button("OK") {
+                            if let imageData = selectedImageData {
+                                let attendee = Attendee(imageData: imageData, attendeeName: attendeeName, location: locationFetcher.lastKnownLocation)
+                                //conferenceList.append(attendee)
+                                attendees.attendees.append(attendee)
+                            }
+                            selectedImageData = nil
+                            attendeeName = "Attendee Name"
+                            showingNameAlert = false
+                        }
                     }
                 }
             }
         }
-    }
 
 }
 
