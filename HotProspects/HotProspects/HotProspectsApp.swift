ad262dde6172b1e42f6e4efb089f5ca3bee4e924
tipperr <ciaranenator@gmail.com>
2024-03-16 15:07:57 -0400

    Added "Result"

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Ciaran Murphy on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- HotProspects—Techniques/HotProspects—Techniques/ContentView.swift
+++ HotProspects—Techniques/HotProspects—Techniques/ContentView.swift
@@ -69,14 +69,43 @@ struct ContentView: View {
  import SwiftUI
 
  struct ContentView: View {
+     @State private var output = ""
+     
      var body: some View {
-         VStack {
-             Image(systemName: "globe")
-                 .imageScale(.large)
-                 .foregroundStyle(.tint)
-             Text("Hello, world!")
+        Text(output)
+             .task {
+                 await fetchReadings()
+             }
+     }
+     
+     func fetchReadings() async {
+         //do {
+         let fetchTask = Task{
+             let url = URL(string: "https://hws.dev/readings.json")!
+             let (data, _) = try await URLSession.shared.data(from: url)
+             let readings = try JSONDecoder().decode([Double].self, from:  data)
+             //output = "Found \(readings.count) readings"
+             return "Found \(readings.count) readings"
          }
-         .padding()
+         
+         let result = await fetchTask.result
+         
+         switch result {
+         case .success(let str):
+             output = str
+         case .failure(let error):
+             output = "Error: \(error.localizedDescription)"
+         }
+         
+         /*do {
+             output = try result.get()
+         } catch {
+             output = "Error: \(error.localizedDescription)"
+         }*/
+         
+         /*catch {
+             print("Download Error")
+         }*/
      }
  }
 
