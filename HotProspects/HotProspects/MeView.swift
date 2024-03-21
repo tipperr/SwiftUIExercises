235b621cb8916ab393d883c7ad6825af792810a8
tipperr <ciaranenator@gmail.com>
2024-03-21 06:45:13 -0400

    Completed HotProspects

--- HotProspects/HotProspects/MeView.swift
+++ HotProspects/HotProspects/MeView.swift
@@ -12,6 +12,8 @@ struct MeView: View {
     @AppStorage("name") private var name = "Anonymous"
     @AppStorage("emailAddress") private var emailAddress = "you@yoursite.com"
     
+    @State private var qrCode = UIImage()
+    
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
     
@@ -26,14 +28,26 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrCode)
                     .interpolation(.none)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu{
+                        /*let image = generateQRCode(from: "\(name)\n\(emailAddress)")*/
+                        
+                        ShareLink(item: Image(uiImage: qrCode), preview: SharePreview("My QR Code", image: Image(uiImage: qrCode)))
+                    }
             }
             .navigationTitle("Your Code")
+            .onAppear(perform: updateCode)
+            .onChange(of: name, updateCode)
+            .onChange(of: emailAddress, updateCode)
         }
+    }
+    
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
     }
     
     func generateQRCode(from string: String) -> UIImage {
--- HotProspects/HotProspects/ProspectsView.swift
+++ HotProspects/HotProspects/ProspectsView.swift
@@ -5,8 +5,10 @@
 //  Created by Ciaran Murphy on 3/19/24.
 //
 
+import CodeScanner
 import SwiftData
 import SwiftUI
+import UserNotifications
 
 struct ProspectsView: View {
     enum FilterType {
@@ -15,6 +17,8 @@ struct ProspectsView: View {
     
     @Environment(\.modelContext) var modelContext
     @Query(sort: \Prospect.name) var prospects: [Prospect]
+    @State private var isShowingScanner = false
+    @State private var selectedProspects = Set<Prospect>()
     
     let filter: FilterType
     
@@ -32,21 +36,63 @@ struct ProspectsView: View {
     var body: some View {
         NavigationStack{
             //Text("People: \(prospects.count)")
-            List(prospects){ prospect in
+            List(prospects, selection: $selectedProspects){ prospect in
                 VStack(alignment: .leading){
                     Text(prospect.name)
                         .font(.headline)
                     Text(prospect.emailAddress)
                         .foregroundStyle(.secondary)
                 }
+                .swipeActions{
+                    Button("Delete", systemImage: "trash", role: .destructive){
+                        modelContext.delete(prospect)
+                    }
+                    
+                    if prospect.isContacted {
+                        Button("Mark Uncontacted", systemImage: "person.crop.circle.badge.xmark"){
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.blue)
+                    } else {
+                        Button("Mark Contactacted", systemImage: "person.crop.circle.fill.badge.checkmark"){
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.green)
+                        
+                        Button("Remind me", systemImage: "bell"){
+                            addNotification(for: prospect)
+                        }
+                        .tint(.orange)
+                    }
+                }
+                .tag(prospect)
             }
                 .navigationTitle(title)
                 .toolbar{
-                    Button("Scan", systemImage: "qrcode.viewfinder"){
-                        let prospect = Prospect(name: "Ciarán Murphy", emailAddress: "Test@Test.Test", isContacted: false)
-                        modelContext.insert(prospect)
+                    ToolbarItem(placement: .topBarTrailing){
+                        Button("Scan", systemImage: "qrcode.viewfinder"){
+                            /*let prospect = Prospect(name: "Ciarán Murphy", emailAddress: "Test@Test.Test", isContacted: false)
+                            modelContext.insert(prospect)*/
+                            isShowingScanner = true
+                        }
                     }
+                    
+                    
+                    ToolbarItem(placement: .topBarLeading){
+                        EditButton()
+                    }
+                    
+                    if selectedProspects.isEmpty == false {
+                        ToolbarItem(placement: .bottomBar){
+                            Button("Delete Selected", action: delete)
+                        }
+                    }
+                    
                 }
+                .sheet(isPresented: $isShowingScanner){
+                    CodeScannerView(codeTypes: [.qr], simulatedData: "Ciarán Murphy\nciaranenator@gmail.com", completion: handleScan)
+                }
+
         }
     }
     
@@ -59,6 +105,63 @@ struct ProspectsView: View {
             _prospects = Query(filter: #Predicate{
                 $0.isContacted == showContactedOnly
             }, sort: [SortDescriptor(\Prospect.name)])
+        }
+    }
+    
+    func delete() {
+        for prospect in selectedProspects {
+            modelContext.delete(prospect)
+        }
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>){
+        isShowingScanner = false
+        
+        switch result {
+        case.success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+            
+            let person = Prospect(name: details[0], emailAddress: details[1], isContacted: false)
+            modelContext.insert(person)
+            
+        case.failure(let error):
+            print("Scanning Failed: \(error.localizedDescription)")
+        }
+    }
+    
+    func addNotification(for prospect: Prospect){
+        let center = UNUserNotificationCenter.current()
+        
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Contact \(prospect.name)"
+            content.subtitle = prospect.emailAddress
+            content.sound = UNNotificationSound.default
+            
+//            var dateComponents = DateComponents()
+//            dateComponents.hour = 9
+//            
+//            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            center.add(request)
+        }
+        
+        center.getNotificationSettings { settings in
+            if settings.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        addRequest()
+                    } else if let error {
+                        print("Error: \(error.localizedDescription)")
+                    }
+                }
+            }
         }
     }
 }
