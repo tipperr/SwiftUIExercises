d4641755517fd35dfd5ffcb2ad91551084a37672
tipperr <ciaranenator@gmail.com>
2024-04-12 12:12:08 -0400

    Started DiceRoller; Added dice sizes

---
+++ DiceRoller/DiceRoller/ContentView.swift
@@ -0,0 +1,85 @@
+//
+//  ContentView.swift
+//  DiceRoller
+//
+//  Created by Ciaran Murphy on 4/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var value = 0
+    
+    @State private var sides = 6
+    
+    @State private var diceNumber = 1
+    
+    @State private var previousRolls = [Int]()
+    
+    var sideOptions = [4, 6, 8, 10, 12, 20, 100]
+    
+    var body: some View {
+        VStack{
+            
+            Spacer()
+            
+            Picker("Sides", selection: $sides){
+                ForEach(sideOptions, id: \.self){
+                    Text("\($0)")
+                }
+            }
+            
+            Picker("Number of dice:", selection: $diceNumber){
+                ForEach(1..<6, id: \.self){
+                    Text("\($0)")
+                }
+            }
+            
+            Text("Number of sides: \(sides)")
+            
+            Text("Number of dice: \(diceNumber)")
+            
+            Text("\(value)")
+                .font(.largeTitle)
+                .padding()
+            
+            Text("\(previousRolls.map { String($0) }.joined(separator: "     "))")
+                .font(.largeTitle)
+                .padding()
+            
+            Button("Roll"){
+                roll(sides: sides)
+                print(previousRolls)
+            }
+            .foregroundStyle(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(.capsule)
+                        
+            List{
+                ForEach(previousRolls.reversed(), id:\.self){
+                    Text("\($0)")
+                }
+            }
+        }
+    }
+    
+//    func roll(sides: Int) {
+//        value = Int.random(in: 1...sides)
+//        previousRolls.append(value)
+//    }
+    
+    func roll(sides: Int) {
+        previousRolls.removeAll() // Clear the previous rolls
+        
+        for _ in 0..<diceNumber {
+            let value = Int.random(in: 1...sides)
+            previousRolls.append(value)
+        }
+    }
+
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Ciaran Murphy on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
