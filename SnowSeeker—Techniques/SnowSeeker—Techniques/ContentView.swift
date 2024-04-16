6f90f13f98b8bcd311f26493ee3e8d16218f570b
tipperr <ciaranenator@gmail.com>
2024-04-16 14:43:05 -0400

    Groups as layout containers

--- SnowSeeker—Techniques/SnowSeeker—Techniques/ContentView.swift
+++ SnowSeeker—Techniques/SnowSeeker—Techniques/ContentView.swift
@@ -11,6 +11,17 @@ struct User: Identifiable {
     var id = "Taylor Swift"
 }
 
+struct UserView: View {
+    var body: some View {
+        Group{
+            Text("Name: Ciarán")
+            Text("Location: NYC")
+            Text("Pets: Orla and Ned")
+        }
+        .font(.title)
+    }
+}
+
 //struct ContentView: View {
 //    var body: some View {
 //        //NavigationSplitView(columnVisibility: .constant(.all)){
@@ -25,22 +36,64 @@ struct User: Identifiable {
 //    }
 //}
 
+//struct ContentView: View {
+//    @State private var selectedUser: User? = nil
+//    @State private var isShowingUser = false
+//    
+//    var body: some View {
+//        Button("Tap me"){
+//            selectedUser = User()
+//            isShowingUser = true
+//        }
+//        .sheet(item: $selectedUser) { user in
+//            Text(user.id)
+//                .presentationDetents([.medium, .large])
+//        }
+////        .alert("Welcome", isPresented: $isShowingUser, presenting: selectedUser){ user in
+////            Button(user.id){
+////                
+////            }
+////        }
+//    }
+//}
+
 struct ContentView: View {
-    @State private var selectedUser: User? = nil
-    @State private var isShowingUser = false
+    @State private var layoutVertically = false
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
     
     var body: some View {
-        Button("Tap me"){
-            selectedUser = User()
-            isShowingUser = true
+        
+        ViewThatFits{
+            Rectangle()
+                .frame(width: 500, height: 200)
+            
+            Circle()
+                .frame(width: 200, height: 200)
         }
-        .sheet(item: $selectedUser) { user in
-            Text(user.id)
-                .presentationDetents([.medium, .large])
-        }
-//        .alert("Welcome", isPresented: $isShowingUser, presenting: selectedUser){ user in
-//            Button(user.id){
-//                
+        
+//        if horizontalSizeClass == .compact {
+//            //if layoutVertically {
+//                VStack{
+//                    UserView()
+//                }
+//            } else {
+//                HStack {
+//                    UserView()
+//                }
+//            }
+        
+        
+//        Button{
+//            layoutVertically.toggle()
+//        } label: {
+//            if layoutVertically {
+//                VStack{
+//                    UserView()
+//                }
+//            } else {
+//                HStack {
+//                    UserView()
+//                }
 //            }
 //        }
     }
