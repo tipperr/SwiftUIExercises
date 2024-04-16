3685547d7f33ccfbbe26c16666c423f5d6e80339
tipperr <ciaranenator@gmail.com>
2024-04-16 09:52:35 -0400

    Started SnowSeeker

---
+++ SnowSeeker—Techniques/SnowSeeker—Techniques/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  SnowSeeker—Techniques
+//
+//  Created by Ciaran Murphy on 4/16/24.
+//
+
+import SwiftUI
+
+//struct ContentView: View {
+//    var body: some View {
+//        //NavigationSplitView(columnVisibility: .constant(.all)){
+//        NavigationSplitView(preferredCompactColumn: .constant(.detail)){
+//            NavigationLink("Primary"){
+//                Text("New View")
+//            }
+//        } detail: {
+//            Text("Content")
+//        }
+//        .navigationSplitViewStyle(.balanced)
+//    }
+//}
+
+struct ContentView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnowSeeker—Techniques/SnowSeeker—Techniques/SnowSeeker_TechniquesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeeker_TechniquesApp.swift
+//  SnowSeeker—Techniques
+//
+//  Created by Ciaran Murphy on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeeker_TechniquesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
